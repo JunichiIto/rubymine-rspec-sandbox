acd909379bed95edc567ff53e64247b32a531038
Junichi Ito <jit@sonicgarden.jp>
2017-02-09 07:46:13 +0900

    Add model spec

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Blog, type: :model do
+  describe 'attributes' do
+    example "should return values" do
+      blog = Blog.create!(title: 'hello', content: 'world')
+      expect(blog).to have_attributes(
+                          title: 'hello',
+                          content: 'world',
+                      )
+    end
+  end
 end
